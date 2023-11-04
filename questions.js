cb509d042e462e9f04bba672228e8fd65b25515b
PabloAsousa <pabloalbertodesousa97@gmail.com>
2023-11-04 01:42:45 -0300

    Responde ai prof

---
+++ questions.js
@@ -0,0 +1,34 @@
+export default [
+    {
+      question: "Qual a Turma preferida de Felipe?",
+      answers: [
+        { option: "Com certeza turma JAVA", correct: true },
+        { option: "Estilistas de botão", correct: false },
+        { option: "Cobol", correct: false },
+      ],
+    },
+    {
+      question: "Qual o melhor anime do mundo?",
+      answers: [
+        { option: "Dragon ball", correct: false },
+        { option: "Berserk", correct: true },
+        { option: "Naruto", correct: false },
+      ],
+    },
+    {
+      question: "Qual a cor do Tubarão Branco?",
+      answers: [
+        { option: "Branco", correct: false },
+        { option: "Rosa", correct: false },
+        { option: "Cinza", correct: true },
+      ],
+    },
+    {
+      question: "Qual o melhor filme já feito?",
+      answers: [
+        { option: "A origem", correct: false },
+        { option: "Bastardos Inglorios", correct: false },
+        { option: "Interstellar", correct: true },
+      ],
+    },
+  ];
